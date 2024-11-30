6bbc9207e00af43e8f276996707f8b85079257d4
harshaa34 <144621014+harshaa34@users.noreply.github.com>
2024-11-30 12:12:39 +0530

    Update Calculator.java
    entire code update

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,29 +2,35 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        //comment is added from repo
-        int x,y,sum;
+        // Comment is added from the repo
+        int x, y, result;
         String operator;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Please enter the 1st number:");
         x = sc.nextInt();
         System.out.println("Please enter the 2nd number:");
-        y =sc.nextInt();
+        y = sc.nextInt();
 
-        System.out.println("Enter the operation to perfom: ");
+        System.out.println("Enter the operation to perform:");
         operator = sc.next();
 
+        result = switch (operator) {
+            case "+" -> {
+                yield x + y;
+            }
+            case "-" -> {
+                yield x - y;
+            }
+            case "*" -> {
+                yield x * y;
+            }
+            case "%" -> {
+                yield x % y;
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + operator);
+        };
 
-       sum = switch (operator) {
-           case "+"-> x+y;
-           case "-"-> x-y;
-           case "*"-> x*y;
-           case "%"-> x%y;
-
-           default -> throw new IllegalStateException("Unexpected value: " + operator);
-       };
-
-        System.out.println("The answer is "+sum);
+        System.out.println("The answer is " + result);
     }
 }
