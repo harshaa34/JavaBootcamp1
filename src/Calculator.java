4b81c0216b0d98285452f8cbb9357403b800ba5a
harshaa34 <144621014+harshaa34@users.noreply.github.com>
2024-11-30 12:05:25 +0530

    Calculator Function is Added

---
+++ src/Calculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int x,y,sum;
+        String operator;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter the 1st number:");
+        x = sc.nextInt();
+        System.out.println("Please enter the 2nd number:");
+        y =sc.nextInt();
+
+        System.out.println("Enter the operation to perfom: ");
+        operator = sc.next();
+
+
+       sum = switch (operator) {
+           case "+"-> x+y;
+           case "-"-> x-y;
+           case "*"-> x*y;
+           case "%"-> x%y;
+
+           default -> throw new IllegalStateException("Unexpected value: " + operator);
+       };
+
+        System.out.println("The answer is "+sum);
+    }
+}
